0cb9a1ae9230b6052ef21a75d7ab28659dc56cfd
agallen <axel.lars.gallen@cern.ch>
2022-10-31 16:19:47 +0100

    Added empty line

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import sys
 import modules.helper as helper
 import modules.models as models
 
+
+
 def main():
     project_path, data_path, config = helper.initialize(sys.argv)
     train_set, test_set, number_of_columns = helper.process(data_path, config)
