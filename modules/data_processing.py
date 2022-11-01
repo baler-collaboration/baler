2cce8fc1279614418cfa823fca06281423612dc8
agallen <axel.lars.gallen@cern.ch>
2022-11-01 18:50:24 +0100

    Added root read function & cms data configs

--- modules/data_processing.py
+++ modules/data_processing.py
@@ -1,14 +1,39 @@
+from matplotlib.style import library
 from sklearn.model_selection import train_test_split
 import json
 import pandas as pd
+import uproot 
 
 def import_config(config_path):
     with open (config_path) as json_config:
         config = json.load(json_config)
     return config
 
+def Type_clearing(TTree):
+    typenames = TTree.typenames()
+    Column_Type = []
+    Column_names = []
+
+    # In order to remove non integers or -floats in the TTree, we separate the values and keys
+    for keys in typenames:
+        Column_Type.append(typenames[keys])
+        Column_names.append(keys)
+ 
+    # Checks each value of the typename values to see if it isn't an int or float, and then removes it
+    for i in range(len(Column_Type)):
+        if Column_Type[i] != 'float[]' and Column_Type[i] != 'int32_t[]':
+            #print("Index ",i," was of type ",Typename_list_values[i]," and was deleted from the file")
+            del Column_names[i]
+    
+    # Returns list of column names to use in load_data function
+    return Column_names
+
 def load_data(data_path,config):
-    df = pd.read_csv(data_path,low_memory=False)
+    if ".csv" in data_path[-4:]:
+        df = pd.read_csv(data_path,low_memory=False)
+    elif ".root" in data_path[-5:]:
+        tree = uproot.open(data_path)[config["Branch"]][config["Collection"]][config["Objects"]]
+        df = tree.arrays(Type_clearing(tree), library="pd")
     return df
 
 def clean_data(df,config):
--- modules/plotting.py
+++ modules/plotting.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 def plot (before,after):
     counts_before, bins_before = np.histogram(before,bins=np.arange(0,200,1))
-    plt.hist(bins_before[:-1], bins_before, weights=counts_before)
+    plt.hist(bins_before[:-1], bins_before, weights=counts_before, label='Before')
 
     counts_after, bins_after = np.histogram(after,bins=np.arange(0,200,1))
-    plt.hist(bins_after[:-1], bins_after, weights=counts_after)
-
+    plt.hist(bins_after[:-1], bins_after, weights=counts_after, label='After',histtype='step')
+    plt.legend(loc="best")
     plt.show()
