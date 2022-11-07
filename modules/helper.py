ec49915b4b3b28cbb2b89f8e2a3a75d88f70f575
Alexander Ekman <per.alexander.ekman@cern.ch>
2022-11-07 18:49:43 +0100

    Added the different running modes

--- modules/helper.py
+++ modules/helper.py
@@ -18,8 +18,9 @@ def get_arguments():
     parser.add_argument('--model', type=str, required=False, help='Path to previously derived machinelearning model')
     parser.add_argument('--input', type=str, required=True, help='Path to input data set for compression')
     parser.add_argument('--output', type=str, required=True, help='Path of output data')
+    parser.add_argument('--mode', type=str, required=True, help='train, compress, decompress, plot')
     args = parser.parse_args()
-    return args.input, args.output, args.model, data_processing.import_config(args.config)
+    return args.input, args.output, args.model, data_processing.import_config(args.config), args.mode
 
 def process(data_path, config):
     df = data_processing.load_data(data_path,config)
--- run.py
+++ run.py
@@ -3,15 +3,17 @@ import modules.helper as helper
 import modules.models as models
 
 def main():
-    input_path, output_path, model, config = helper.get_arguments()
-    train_set, test_set, number_of_columns = helper.process(input_path, config)
+    input_path, output_path, model, config, mode = helper.get_arguments()
 
-    model = models.george_SAE(n_features=number_of_columns, z_dim=25)
+    if mode == "train":
+        train_set, test_set, number_of_columns = helper.process(input_path, config)
+        model = models.george_SAE(n_features=number_of_columns, z_dim=25)
+        test_data, reconstructed_data = helper.train(model,number_of_columns,train_set,test_set,output_path,config)
+        test_data_renorm = helper.undo_normalization(test_data,test_set,train_set,config)
+        reconstructed_data_renorm = helper.undo_normalization(reconstructed_data,test_set,train_set,config)
 
-    test_data, reconstructed_data = helper.train(model,number_of_columns,train_set,test_set,output_path,config)
-    test_data_renorm = helper.undo_normalization(test_data,test_set,train_set,config)
-    reconstructed_data_renorm = helper.undo_normalization(reconstructed_data,test_set,train_set,config)
-    helper.plot(test_data_renorm, reconstructed_data_renorm)
+    elif mode == "plot":
+        helper.plot(test_data_renorm, reconstructed_data_renorm)
 
 if __name__ == "__main__":
     main()
