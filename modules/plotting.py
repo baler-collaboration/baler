38fe8d9b47291a316424575703a41deb004a6f91
Alexander Ekman <per.alexander.ekman@cern.ch>
2022-11-07 19:30:34 +0100

    Implremented iterative pdf plotting. Not response yet

--- modules/plotting.py
+++ modules/plotting.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
+import modules.data_processing as data_processing
+from matplotlib.backends.backend_pdf import PdfPages
 
 def plot (before_path,after_path):
     with open(before_path, 'rb') as handle:
@@ -8,10 +10,24 @@ def plot (before_path,after_path):
     with open(after_path, 'rb') as handle:
         after = pickle.load(handle)
 
-    counts_before, bins_before = np.histogram(before,bins=np.arange(0,200,1))
-    plt.hist(bins_before[:-1], bins_before, weights=counts_before, label='Before')
+    columns = data_processing.get_columns(before)
+    number_of_columns = len(columns)
 
-    counts_after, bins_after = np.histogram(after,bins=np.arange(0,200,1))
-    plt.hist(bins_after[:-1], bins_after, weights=counts_after, label='After',histtype='step')
-    plt.legend(loc="best")
-    plt.show()
+    figure, ax1 = plt.subplots(figsize=(18.3*(1/2.54)*1.7, 13.875*(1/2.54)*1.32))
+    with PdfPages(after_path.split("after.pickle")[0]+"comparison.pdf") as pdf:
+        for index, column in enumerate(columns):
+            print(f'{index} of {number_of_columns}')
+            counts_before, bins_before = np.histogram(before[column],bins=np.arange(0,200,1))
+            ax1.hist(bins_before[:-1], bins_before, weights=counts_before, label='Before')
+        
+            counts_after, bins_after = np.histogram(after[column],bins=np.arange(0,200,1))
+            ax1.hist(bins_after[:-1], bins_after, weights=counts_after, label='After',histtype='step')
+
+            ax1.set_title(f"{column}")
+            ax1.set_xlabel(column, ha='right', x=1.0)
+            ax1.set_ylabel("Counts", ha='right', y=1.0)
+
+            ax1.legend(loc="best")
+            pdf.savefig()
+            ax1.clear()
+            #if index==10: break
