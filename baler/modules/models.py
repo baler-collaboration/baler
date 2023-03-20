094b3a88d6475d367251a0d3c5bd16a22470c12a
Marta Camps Santasmasas <marta.campssantasmasas@postgrad.manchester.ac.uk>
2023-03-20 15:18:27 +0000

    Added CFD convolution model

--- baler/modules/models.py
+++ baler/modules/models.py
@@ -224,3 +224,77 @@ class george_SAE_Dropout(nn.Module):
             l1_loss += torch.mean(torch.abs(values))
         loss = mse_loss + reg_param * l1_loss
         return loss
+        
+class Conv_AE(nn.Module):
+        def __init__(self, n_features, z_dim, *args, **kwargs):
+            super(Conv_AE, self).__init__(*args, **kwargs)
+            self.q_z_mid_dim = 2000
+            self.q_z_output_dim = 72128
+            # Encoder
+            # Conv Layers
+            self.q_z_conv = nn.Sequential(
+                  nn.Conv2d(1, 8, kernel_size=(2,5), stride=(1), padding=(1)),
+                  #nn.BatchNorm2d(8),
+                  nn.ReLU(),
+                  nn.Conv2d(8, 16, kernel_size=(3), stride=(1), padding=(1)),
+                  nn.BatchNorm2d(16),
+                  nn.ReLU(),
+                  nn.Conv2d(16, 32, kernel_size=(3), stride=(1), padding=(0)),
+                  #nn.BatchNorm2d(32),
+                  nn.ReLU()
+                  ) 
+            # Flatten
+            self.flatten = nn.Flatten(start_dim=1)
+            # Linear layers
+            self.q_z_lin = nn.Sequential( 
+                  nn.Linear(self.q_z_output_dim, self.q_z_mid_dim), 
+                  nn.ReLU(), 
+                  # nn.BatchNorm1d(self.q_z_output_dim),
+                  nn.Linear(self.q_z_mid_dim, z_dim),
+                  nn.ReLU(),
+                  )
+
+            # Decoder
+            self.p_x_lin = nn.Sequential(
+                  nn.Linear(z_dim, self.q_z_mid_dim),
+                  nn.ReLU(),
+                  # nn.BatchNorm1d(self.q_z_output_dim),
+                  nn.Linear(self.q_z_mid_dim, self.q_z_output_dim),
+                  nn.ReLU(),
+                  # nn.BatchNorm1d(42720) 
+                  )
+            # Conv Layers
+            self.p_x_conv = nn.Sequential(
+                  nn.ConvTranspose2d(32, 16, kernel_size=(3), stride=(1), padding=(0)),
+                  nn.BatchNorm2d(16),
+                  nn.ReLU(),
+                  nn.ConvTranspose2d(16, 8, kernel_size=(3), stride=(1), padding=(1)),
+                  nn.BatchNorm2d(8),
+                  nn.ReLU(),
+                  nn.ConvTranspose2d(8, 1, kernel_size=(2,5), stride=(1), padding=(1))
+                  ) 
+    
+        def encode(self, x):
+            # Conv
+            out = self.q_z_conv(x)
+            # flatten
+            out = self.flatten(out)
+            #out = out.view(out.size(0), -1)
+            # dense 
+            out = self.q_z_lin(out)
+            return out
+    
+        def decode(self, z):
+            # dense
+            out = self.p_x_lin(z)
+            # reshape
+            out = out.view(out.size(0), 32, 49, 46)  
+            # DeConv/UnConv?
+            out = self.p_x_conv(out)
+            return out
+        
+        def forward(self, x):
+            z = self.encode(x)
+            out = self.decode(z)
+            return out
+
