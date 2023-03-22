bf9493c77d246b3ee844ff15fe88fc517c03da4a
Alexander Ekman <alexander.ekman95@gmail.com>
2023-03-22 10:17:11 +0100

    full workfolw works for both CFD and CMS data

--- baler/modules/helper.py
+++ baler/modules/helper.py
@@ -4,6 +4,7 @@ import os
 import pickle
 from dataclasses import dataclass
 import sys
+
 sys.path.append(os.getcwd())
 
 import numpy as np
@@ -16,14 +17,16 @@ from modules import training, plotting, data_processing
 def get_arguments():
     parser = argparse.ArgumentParser(
         prog="baler.py",
-        description=("Baler is a machine learning based compression tool for big data.\n\n"
-                     "Baler has three running modes:\n\n"
-                     "\t1. Derivation: Using a configuration file and a \"small\" input dataset, Baler derives a "
-                     "machine learning model optimized to compress and decompress your data.\n\n"
-                     "\t2. Compression: Using a previously derived model and a large input dataset, Baler compresses "
-                     "your data and outputs a smaller compressed file.\n\n"
-                     "\t3. Decompression: Using a previously compressed file as input and a model, Baler decompresses "
-                     "your data into a larger file."),
+        description=(
+            "Baler is a machine learning based compression tool for big data.\n\n"
+            "Baler has three running modes:\n\n"
+            '\t1. Derivation: Using a configuration file and a "small" input dataset, Baler derives a '
+            "machine learning model optimized to compress and decompress your data.\n\n"
+            "\t2. Compression: Using a previously derived model and a large input dataset, Baler compresses "
+            "your data and outputs a smaller compressed file.\n\n"
+            "\t3. Decompression: Using a previously compressed file as input and a model, Baler decompresses "
+            "your data into a larger file."
+        ),
         epilog="Enjoy!",
     )
     parser.add_argument(
@@ -45,7 +48,7 @@ def get_arguments():
     else:
         config = Config
         importlib.import_module(
-           f"projects.{args.project}.{args.project}_config"
+            f"projects.{args.project}.{args.project}_config"
         ).set_config(config)
     return config, args.mode, args.project
 
@@ -212,19 +215,24 @@ def compress(model_path, config):
     data = normalize(data_before, config.custom_norm)
     number_of_columns = 0
     try:
-        if config.data_dimension==1:
+        if config.data_dimension == 1:
             column_names = np.load(config.names_path)
             number_of_columns = len(column_names)
             config.latent_space_size = int(
                 number_of_columns // config.compression_ratio
             )
             config.number_of_columns = number_of_columns
-        elif config.data_dimension==2:
+        elif config.data_dimension == 2:
             data = np.load(config.data_path)
             number_of_columns = len(data)
-            config.latent_space_size = int((number_of_columns*number_of_columns) // config.compression_ratio)
+            config.latent_space_size = int(
+                (number_of_columns * number_of_columns) // config.compression_ratio
+            )
         else:
-            raise NameError("Data dimension can only be 1 or 2. Introduced value = " + str(config.data_dimension))
+            raise NameError(
+                "Data dimension can only be 1 or 2. Introduced value = "
+                + str(config.data_dimension)
+            )
     except AttributeError:
         assert number_of_columns == config.number_of_columns
 
@@ -237,7 +245,10 @@ def compress(model_path, config):
         z_dim=config.latent_space_size,
     )
 
-    data_tensor = torch.from_numpy(data).to(model.device).view(1, 1, 50, 50)
+    if config.data_dimension == 2:
+        data_tensor = torch.from_numpy(data).to(model.device).view(1, 1, 50, 50)
+    elif config.data_dimension == 1:
+        data_tensor = torch.from_numpy(data).to(model.device)
 
     compressed = model.encode(data_tensor)
     return compressed, data_before
--- baler/modules/plotting.py
+++ baler/modules/plotting.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
+from matplotlib.backends.backend_pdf import PdfPages
 
 
 # pickle_file = "./data/cfd2/cfd.pickle"
@@ -48,8 +49,152 @@ def pickle_to_df(file):
         return data
 
 
-def plot(project_path, config):
+def get_index_to_cut(column_index, cut, array):
+    indices_to_cut = np.argwhere(array[column_index] < cut).flatten()
+    return indices_to_cut
 
+
+def plot_1D(project_path, config):
+    output_path = project_path + "training/"
+    names_path = config.names_path
+    before_path = config.data_path
+    after_path = project_path + "decompressed_output/decompressed.npy"
+
+    before = np.transpose(np.load(before_path))
+    after = np.transpose(np.load(after_path))
+    names = np.load(names_path)
+
+    index_to_cut = get_index_to_cut(3, 1e-6, before)
+    before = np.delete(before, index_to_cut, axis=1)
+    after = np.delete(after, index_to_cut, axis=1)
+
+    response = np.divide(np.subtract(after, before), before) * 100
+    residual = np.subtract(after, before)
+
+    with PdfPages(project_path + "/plotting/comparison.pdf") as pdf:
+        fig = plt.figure(constrained_layout=True, figsize=(10, 4))
+        subfigs = fig.subfigures(1, 2, wspace=0.07, width_ratios=[1, 1])
+
+        axsLeft = subfigs[0].subplots(2, 1, sharex=True)
+        ax1 = axsLeft[0]
+        ax3 = axsLeft[1]
+        axsRight = subfigs[1].subplots(2, 1, sharex=False)
+        ax2 = axsRight[0]
+        ax4 = axsRight[1]
+
+        number_of_columns = len(names)
+        for index, column in enumerate(names):
+            column_name = column.split(".")[-1]
+            print(f"Plotting: {column_name} ({index+1} of {number_of_columns})")
+            rms = np.sqrt(np.mean(np.square(response[index])))
+            residual_RMS = np.sqrt(np.mean(np.square(residual[index])))
+
+            x_min = min(before[index] + after[index])
+            x_max = max(before[index] + after[index])
+            x_diff = abs(x_max - x_min)
+
+            # Before Histogram
+            counts_before, bins_before = np.histogram(
+                before[index],
+                bins=np.linspace(x_min - 0.1 * x_diff, x_max + 0.1 * x_diff, 200),
+            )
+            ax1.hist(
+                bins_before[:-1], bins_before, weights=counts_before, label="Before"
+            )
+
+            # After Histogram
+            counts_after, bins_after = np.histogram(
+                after[index],
+                bins=np.linspace(x_min - 0.1 * x_diff, x_max + 0.1 * x_diff, 200),
+            )
+            ax1.hist(
+                bins_after[:-1],
+                bins_after,
+                weights=counts_after,
+                label="After",
+                histtype="step",
+            )
+
+            ax1.set_ylabel("Counts", ha="right", y=1.0)
+            ax1.set_yscale("log")
+            ax1.legend(loc="best")
+            ax1.set_xlim(x_min - 0.1 * x_diff, x_max + 0.1 * x_diff)
+            ax1.set_ylim(ymin=1)
+
+            data_bin_centers = bins_after[:-1] + (bins_after[1:] - bins_after[:-1]) / 2
+            ax3.scatter(
+                data_bin_centers, (counts_after - counts_before), marker="."
+            )  # FIXME: Dividing by zero
+            ax3.axhline(y=0, linewidth=0.2, color="black")
+            ax3.set_xlabel(f"{column_name}", ha="right", x=1.0)
+            ax3.set_ylim(
+                -max(counts_after - counts_before)
+                - 0.05 * max(counts_after - counts_before),
+                max(counts_after - counts_before)
+                + 0.05 * max(counts_after - counts_before),
+            )
+            ax3.set_ylabel("Residual")
+
+            # Response Histogram
+            counts_response, bins_response = np.histogram(
+                response[index], bins=np.arange(-20, 20, 0.2)
+            )
+            ax2.hist(
+                bins_response[:-1],
+                bins_response,
+                weights=counts_response,
+                label="Response",
+            )
+            ax2.axvline(
+                np.mean(response[index]),
+                color="k",
+                linestyle="dashed",
+                linewidth=1,
+                label=f"Mean {round(np.mean(response[index]),4)} %",
+            )
+            ax2.plot([], [], " ", label=f"RMS: {round(rms,4)} %")
+
+            ax2.set_xlabel(f"{column_name} Response [%]", ha="right", x=1.0)
+            ax2.set_ylabel("Counts", ha="right", y=1.0)
+            ax2.legend(loc="best", bbox_to_anchor=(1, 1.05))
+
+            # Residual Histogram
+            counts_residual, bins_residual = np.histogram(
+                residual[index], bins=np.arange(-1, 1, 0.01)
+            )
+            ax4.hist(
+                bins_residual[:-1],
+                bins_residual,
+                weights=counts_residual,
+                label="Residual",
+            )
+            ax4.axvline(
+                np.mean(residual[index]),
+                color="k",
+                linestyle="dashed",
+                linewidth=1,
+                label=f"Mean {round(np.mean(residual[index]),6)}",
+            )
+            ax4.plot([], [], " ", label=f"RMS: {round(residual_RMS,6)}")
+            ax4.plot([], [], " ", label=f"Max: {round(max(residual[index]),6)}")
+            ax4.plot([], [], " ", label=f"Min: {round(min(residual[index]),6)}")
+
+            ax4.set_xlabel(f"{column_name} Residual", ha="right", x=1.0)
+            ax4.set_ylabel("Counts", ha="right", y=1.0)
+            ax4.set_xlim(-1, 1)
+            ax4.legend(loc="best", bbox_to_anchor=(1, 1.05))
+
+            pdf.savefig()
+            ax2.clear()
+            ax1.clear()
+            ax3.clear()
+            ax4.clear()
+
+            # if index == 3:
+            #    break
+
+
+def plot_2D(project_path, config):
     data = np.load(config.data_path)
 
     data_decompressed = np.load(
@@ -87,3 +232,10 @@ def plot(project_path, config):
     # fig.suptitle('Compressed file is 10% the size of original,\n500 epochs (20 min)',y=0.9, fontsize=16)
 
     fig.savefig(project_path + "/plotting/" + "CFD.jpg", bbox_inches="tight")
+
+
+def plot(project_path, config):
+    if config.data_dimension == 1:
+        plot_1D(project_path, config)
+    elif config.data_dimension == 2:
+        plot_2D(project_path, config)
