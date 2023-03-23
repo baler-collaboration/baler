0a43d9da958e1adb184fee90e5d4e851bb64d785
Fritjof Bengtsson <fritjof.folkeson@gmail.com>
2023-03-23 10:33:53 +0100

    update test to run entire chain of events

--- bin/test_docker.sh
+++ bin/test_docker.sh
@@ -3,7 +3,7 @@
 # This script servers as a wrapper around docker run to run the docker image
 # It also tests the docker image
 
-
+PROJECT_NAME="example_CMS"
 DOCKERFILE="Dockerfile"
 ARCH="amd64"
 
@@ -16,5 +16,13 @@ fi
 # Run the docker image with --mode=train and --project=example_CMS
 # We need to mount the projects directory and the data directory
 docker run -v $(pwd)/projects:/baler-root/projects \
-           -v $(pwd)/data:/baler-baler/data \
-           baler-${ARCH}:latest --mode=train --project=example_CMS
+           -v $(pwd)/data:/baler-root/data \
+           baler-${ARCH}:latest --mode=train --project="${PROJECT_NAME}"
+
+docker run -v $(pwd)/projects:/baler-root/projects \
+           -v $(pwd)/data:/baler-root/data \
+           baler-${ARCH}:latest --mode=compress --project="${PROJECT_NAME}"
+
+docker run -v $(pwd)/projects:/baler-root/projects \
+           -v $(pwd)/data:/baler-root/data \
+           baler-${ARCH}:latest --mode=decompress --project="${PROJECT_NAME}"
