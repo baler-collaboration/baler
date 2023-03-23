950011f9e125f777de3e3ae297fa5efe157d1771
Pratik Jawahar <prjawaha@lxplus903.cern.ch>
2023-03-23 16:18:13 +0100

    cosmetic changes to config creation

--- baler/modules/helper.py
+++ baler/modules/helper.py
@@ -98,28 +98,28 @@ class Config:
 def create_default_config(project_name: str) -> str:
     return f"""
 def set_config(c):
-    c.input_path          = "data/{project_name}/{project_name}_data.npz"
-    c.compression_ratio   = 2.0
-    #c.number_of_columns = 24
-    #c.latent_space_size = 12
-    c.epochs              = 5
-    c.early_stopping      = True
-    c.early_stopping_patience            = 100
-    c.min_delta           = 0
-    c.lr_scheduler        = False
-    c.lr_scheduler_patience            = 100
-    c.model_name          = "AE"
-    c.custom_norm         = False
-    c.l1                  = True
-    c.reg_param             = 0.001
-    c.RHO                 = 0.05
-    c.lr                  = 0.001
-    c.batch_size          = 512
-    c.test_size           = 0.15
-    c.data_dimension      = 1
-    c.apply_normalization = True
-    c.extra_compression   = False
-    c.intermittent_model_saving = False
+    c.input_path                   = "data/{project_name}/{project_name}_data.npz"
+    c.compression_ratio            = 2.0
+    # c.number_of_columns           = 24
+    # c.latent_space_size           = 12
+    c.epochs                       = 5
+    c.early_stopping               = True
+    c.early_stopping_patience      = 100
+    c.min_delta                    = 0
+    c.lr_scheduler                 = False
+    c.lr_scheduler_patience        = 100
+    c.model_name                   = "AE"
+    c.custom_norm                  = False
+    c.l1                           = True
+    c.reg_param                    = 0.001
+    c.RHO                          = 0.05
+    c.lr                           = 0.001
+    c.batch_size                   = 512
+    c.test_size                    = 0.15
+    c.data_dimension               = 1
+    c.apply_normalization          = True
+    c.extra_compression            = False
+    c.intermittent_model_saving    = False
     c.intermittent_saving_patience = 100
 
 """
