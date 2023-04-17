c7a0c66c0eed35277906741be780cc303ed1c1d8
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2023-04-17 13:33:23 +0100

    Change docker run command in test and add new example zip

--- bin/test_docker.sh
+++ bin/test_docker.sh
@@ -16,11 +16,18 @@ fi
 # TODO Fix this
 # Run the docker image with --mode=train and --project=example_CMS
 # We need to mount the projects directory and the data directory
-docker run -v $(pwd)/projects:/baler-root/projects \
+
+wget https://cernbox.cern.ch/remote.php/dav/public-files/fOPNAJcX5qgF0Dw/workspaces.zip
+unzip workspaces.zip
+
+docker run -v $(pwd)/workspaces:/baler-root/workspaces \
            baler-${ARCH}:latest --mode train --project "${WORKSPACE_NAME}" "${PROJECT_NAME}"
 
-docker run -v $(pwd)/projects:/baler-root/projects \
+docker run -v $(pwd)/workspaces:/baler-root/workspaces \
+           baler-${ARCH}:latest --mode train --project "${WORKSPACE_NAME}" "${PROJECT_NAME}"
+
+docker run -v $(pwd)/workspaces:/baler-root/workspaces \
            baler-${ARCH}:latest --mode compress --project "${WORKSPACE_NAME}" "${PROJECT_NAME}"
 
-docker run -v $(pwd)/projects:/baler-root/projects \
+docker run -v $(pwd)/workspaces:/baler-root/workspaces \
            baler-${ARCH}:latest --mode decompress --project "${WORKSPACE_NAME}" "${PROJECT_NAME}"
