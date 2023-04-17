ea98d90fe118a6e6f3970e9a09ae4d46bf2fbcef
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2023-04-17 09:55:26 +0100

    Update actions docker test to use workspace

--- bin/test_docker.sh
+++ bin/test_docker.sh
@@ -3,6 +3,7 @@
 # This script servers as a wrapper around docker run to run the docker image
 # It also tests the docker image
 
+WORKSPACE_NAME="CMS_example"
 PROJECT_NAME="example_CMS"
 DOCKERFILE="Dockerfile"
 ARCH="amd64"
@@ -16,13 +17,10 @@ fi
 # Run the docker image with --mode=train and --project=example_CMS
 # We need to mount the projects directory and the data directory
 docker run -v $(pwd)/projects:/baler-root/projects \
-           -v $(pwd)/data:/baler-root/data \
-           baler-${ARCH}:latest --mode=train --project="${PROJECT_NAME}"
+           baler-${ARCH}:latest --mode train --project "${WORKSPACE_NAME}" "${PROJECT_NAME}"
 
 docker run -v $(pwd)/projects:/baler-root/projects \
-           -v $(pwd)/data:/baler-root/data \
-           baler-${ARCH}:latest --mode=compress --project="${PROJECT_NAME}"
+           baler-${ARCH}:latest --mode compress --project "${WORKSPACE_NAME}" "${PROJECT_NAME}"
 
 docker run -v $(pwd)/projects:/baler-root/projects \
-           -v $(pwd)/data:/baler-root/data \
-           baler-${ARCH}:latest --mode=decompress --project="${PROJECT_NAME}"
+           baler-${ARCH}:latest --mode decompress --project "${WORKSPACE_NAME}" "${PROJECT_NAME}"
