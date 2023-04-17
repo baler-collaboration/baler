b0b8eeb3f944b24bdc1bd834835a80242cc71741
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2023-04-17 16:37:18 +0100

    Update tests to use workspace structure

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -19,26 +19,33 @@ from baler.modules import helper
 
 
 def test_create_new_project():
+    workspace_name = "test_workspace"
     project_name = "test_project"
-    base_path = "./tmp"
-    project_path = os.path.join(base_path, project_name)
+    base_path = os.path.join("workspaces")
+    workspace_path = os.path.join(base_path, workspace_name)
+    project_path = os.path.join(workspace_path, project_name)
 
     # Ensure the project doesn't exist yet
-    if os.path.exists(project_path):
-        shutil.rmtree(project_path)
+    if os.path.exists(workspace_path):
+        shutil.rmtree(workspace_path)
 
-    helper.create_new_project(project_name, base_path)
+    helper.create_new_project(workspace_name, project_name)
 
     # Verify that the project was created successfully
+    assert os.path.exists(workspace_path)
     assert os.path.exists(project_path)
-    for directory in [
-        "compressed_output",
-        "decompressed_output",
-        "plotting",
-        "training",
-        "model",
-    ]:
-        assert os.path.exists(os.path.join(project_path, directory))
+
+    required_directories = [
+        os.path.join(workspace_path, "data"),
+        os.path.join(project_path, "config"),
+        os.path.join(project_path, "output", "compressed_output"),
+        os.path.join(project_path, "output", "decompressed_output"),
+        os.path.join(project_path, "output", "plotting"),
+        os.path.join(project_path, "output", "training"),
+    ]
+    for directory in required_directories:
+        print(directory)
+        assert os.path.exists(directory)
 
     # Clean up after the test
     shutil.rmtree(base_path)
