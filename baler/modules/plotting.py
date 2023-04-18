3f296321f4f84501da4bcaeeec158c5596f9553f
Fritjof Bengtsson <fritjof.folkeson@gmail.com>
2023-04-18 13:02:01 +0200

    add box and whisker plot

--- baler/modules/plotting.py
+++ baler/modules/plotting.py
@@ -97,6 +97,7 @@ def plot_1D(project_path, config):
     residual = np.subtract(after, before)
 
     with PdfPages(project_path + "/plotting/comparison.pdf") as pdf:
+        plot_box_and_whisker(names, residual, pdf)
         fig = plt.figure(constrained_layout=True, figsize=(10, 4))
         subfigs = fig.subfigures(1, 2, wspace=0.07, width_ratios=[1, 1])
 
@@ -218,6 +219,29 @@ def plot_1D(project_path, config):
             ax4.clear()
 
 
+def plot_box_and_whisker(names, residual, pdf):
+    """Plots Box and Whisker plots of 1D data
+    Args:
+        project_path (string): The path to the project directory
+        config (dataclass): The config class containing attributes set in the config file
+    """
+    column_names = [name.split(".")[-1] for name in names]
+    fig1, ax1 = plt.subplots()
+
+    boxes = ax1.boxplot(list(residual), showfliers=False, vert=False)
+    whiskers = np.concatenate([item.get_xdata() for item in boxes["whiskers"]])
+    edges = max([abs(min(whiskers)), abs(max(whiskers))])
+
+    ax1.set_yticks(np.arange(1, len(column_names) + 1, 1))
+    ax1.set_yticklabels(column_names)
+
+    ax1.grid()
+    fig1.tight_layout()
+    ax1.set_xlabel("Residual")
+    ax1.set_xlim(-edges - edges * 0.1, edges + edges * 0.1)
+    pdf.savefig()
+
+
 def plot_2D(project_path, config):
     """General plotting for 2D data, for example 2D arraysfrom computational fluid
         dynamics or other image like data. This function generates a pdf
@@ -270,7 +294,7 @@ def plot_2D(project_path, config):
 
         axs[2].set_title("Relative Diff. [%]", fontsize=11, y=-0.2)
         im3 = axs[2].imshow(
-            diff, vmin=-10, vmax=10, cmap="cool_r", interpolation="nearest"
+            diff, vmin=-50, vmax=50, cmap="cool_r", interpolation="nearest"
         )
         cb2 = plt.colorbar(im3, ax=[axs[2]], location="top")
 
