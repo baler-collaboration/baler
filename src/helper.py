739c514a757b23d1bad49eedaf2aea3bf36a0be0
Alexander Ekman <alexander.ekman95@gmail.com>
2024-01-15 14:29:04 +0100

    Added scripts and directories for example

---
+++ src/helper.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+def plot2D(original, reconstructed):
+    plt.close()
+    diff = original - reconstructed
+
+    max_value = np.amax([np.amax(original), np.amax(reconstructed)])
+    min_value = np.amin([np.amin(original), np.amin(reconstructed)])
+
+    fig, axs = plt.subplots(
+        1, 3, figsize=(29.7 * (1 / 2.54), 10 * (1 / 2.54)), sharey=False
+    )
+    axs[0].set_title("Original", fontsize=11)
+    im1 = axs[0].imshow(original, vmax=max_value, vmin=min_value)
+    axs[0].invert_yaxis()
+
+    axs[1].set_title("Reconstructed", fontsize=11)
+    im2 = axs[1].imshow(reconstructed, vmax=max_value, vmin=min_value)
+    axs[1].invert_yaxis()
+
+    axs[2].set_title("Difference", fontsize=11)
+    im3 = axs[2].imshow(diff, vmax=max_value, vmin=min_value)
+    axs[2].invert_yaxis()
+
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.815, 0.2, 0.02, 0.59])
+    cb2 = fig.colorbar(im3, cax=cbar_ax, aspect=10)
+    return fig
+
+
+def loss_plot(loss_data, output_path, config):
+    """This function Plots the loss from the training and saves it
+
+    Args:
+        path_to_loss_data (string): Path to file containing loss plot data generated during training
+        output_path (path): Directory path to which the loss plot is saved
+        config (dataclass): The config class containing attributes set in the config file
+    """
+    # loss_data = np.load(path_to_loss_data)
+    str_list = ["Epochs:", "Model Name:", "Reg. Param:", "lr:", "BS:"]
+
+    train_loss = loss_data[0]
+    val_loss = loss_data[1]
+    conf_list = [
+        len(train_loss),
+        config.model_name,
+        config.reg_param,
+        config.lr,
+        config.batch_size,
+    ]
+
+    plt.figure(figsize=(10, 7))
+    plt.title("Loss plot")
+    plt.plot(train_loss, color="orange", label="Train Loss")
+    if config.test_size:
+        plt.plot(val_loss, color="red", label="Validation Loss")
+    for i in range(len(conf_list)):
+        plt.plot([], [], " ", label=str_list[i] + " " + str(conf_list[i]))
+    plt.xlabel("Epochs")
+    plt.yscale("log")
+    plt.ylabel("Loss")
+    plt.legend(loc="best")
+    plt.savefig(os.path.join(output_path, "Loss_plot.png"))
+    # plt.show()
---
+++ src/run.py
@@ -0,0 +1,168 @@
+import baler_compressor.config as config_module
+import baler_compressor.trainer as trainer_module
+import baler_compressor.compressor as compressor_module
+import baler_compressor.decompressor as decompressor_module
+
+# import helper
+import torch
+import numpy as np
+
+import sys
+import helper
+import imageio.v2 as imageio
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+
+def define_config():
+    # Initialize config
+    config = config_module.Config
+
+    # Define config
+    config.input_path = "./input/219_small.npz"
+    config.output_path = "./output/"
+    config.compression_ratio = 100
+    config.epochs = 2
+    config.early_stopping = False
+    config.early_stopping_patience = 100
+    config.min_delta = 0
+    config.lr_scheduler = True
+    config.lr_scheduler_patience = 100
+    config.model_name = "Conv_AE"
+    config.model_type = "convolutional"
+    config.custom_norm = False
+    config.l1 = True
+    config.reg_param = 0.001
+    config.RHO = 0.05
+    config.lr = 0.001
+    config.batch_size = 200
+    config.test_size = 0.0
+    config.data_dimension = 2
+    config.apply_normalization = False
+    config.deterministic_algorithm = False
+    config.compress_to_latent_space = False
+    config.convert_to_blocks = [1, 50, 50]
+    config.verbose = False
+
+    # FPGA stuff
+    config.number_of_columns = 22500  # FIXME: this is doesn't need to be hardcoded
+    config.latent_space_size = 225  # FIXME: this is doesn't need to be hardcoded
+    config.default_reuse_factor = 1
+    config.default_precision = "ap_fixed<16,8>"
+    config.Strategy = "latency"
+    config.Part = "xcvu9p-flga2104-2L-e"
+    config.ClockPeriod = 5
+    config.IOType = "io_parallel"
+    config.InputShape = (1, 16)
+    config.ProjectName = "tiny_test_model"
+    config.OutputDir = "workspaces/FPGA_compression_workspace/first_FPGA_Compression_project/output/hls4ml"
+    config.InputData = None
+    config.OutputPredictions = None
+    config.csim = False
+    config.synth = True
+    config.cosim = False
+    config.export = False
+
+    return config
+
+
+def train(config):
+    # Run training
+    # (20, 2162, 2068)
+    model, normalization_features, loss_data = trainer_module.run(
+        config.input_path, config
+    )
+    torch.save(model.state_dict(), config.output_path + "compressed_output/model.pt")
+    np.save(
+        config.output_path + "compressed_output/normalization_features.npy",
+        normalization_features,
+    )
+    helper.loss_plot(loss_data[0], config.output_path, config)
+
+
+def compress(config):
+    # Run compression
+    normalization_features = np.load(
+        config.output_path + "compressed_output/normalization_features.npy"
+    )
+    compressed, names, original_shape = compressor_module.run(
+        config.input_path,
+        config.output_path + "compressed_output/model.pt",
+        normalization_features,
+        config,
+    )
+
+    # Save compressed file to disk
+    np.savez_compressed(
+        config.output_path + "compressed_output/compressed.npz",
+        data=compressed,
+        names=names,
+        normalization_features=normalization_features,
+        original_shape=original_shape,
+    )
+    # np.save(config.output_path + "loss_data.npy", loss_data)
+
+
+def decompress(config):
+    # Run decompression
+    decompressed, names, original_shape = decompressor_module.run(
+        config.output_path + "compressed_output/model.pt",
+        config.output_path + "compressed_output/compressed.npz",
+        config,
+    )
+
+    # Save decompressed file to disk
+    np.savez(
+        config.output_path + "decompressed_output/decompressed.npz",
+        data=decompressed,
+        names=names,
+    )
+
+
+def plot(config):
+    data = np.load(config.input_path)["data"]
+    data_decompressed = np.load(
+        config.output_path + "decompressed_output/decompressed.npz"
+    )["data"].reshape(data.shape[0], data.shape[1], data.shape[2])
+
+    print("Making GIF: ")
+    with imageio.get_writer(config.output_path + "/movie.gif", mode="I") as writer:
+        for i in tqdm(range(0, 200)):
+            figg = helper.plot2D(data[i], data_decompressed[i])
+            image_name = config.output_path + f"plot_output/{i}.png"
+            plt.savefig(image_name)
+            image = imageio.imread(image_name)
+            writer.append_data(image)
+
+
+def FPGA(config):
+    import baler_compressor.helper as baler_helper
+
+    baler_helper.perform_hls4ml_conversion(config.output_path, config)
+
+
+def main():
+    config = define_config()
+
+    if sys.argv[1] == "train":
+        train(config)
+    elif sys.argv[1] == "compress":
+        compress(config)
+    elif sys.argv[1] == "decompress":
+        decompress(config)
+    elif sys.argv[1] == "plot":
+        plot(config)
+    elif sys.argv[1] == "FPGA":
+        FPGA(config)
+    elif sys.argv[1] == "all":
+        train(config)
+        compress(config)
+        decompress(config)
+        plot(config)
+        # FPGA(config)
+    else:
+        print("Unknown argument")
+
+
+if __name__ == "__main__":
+    main()
