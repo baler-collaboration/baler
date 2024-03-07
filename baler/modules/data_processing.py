857aee39ea8e4c68322e8931ead997a692ff096e
axelgallen <axel.gallen@physics.uu.se>
2024-03-07 15:45:27 +0000

    Fixed model_saving function to check what state dict save option should be used

--- baler/modules/data_processing.py
+++ baler/modules/data_processing.py
@@ -57,7 +57,9 @@ def encoder_saver(model, model_path: str) -> None:
     Returns:
         None: Saved encoder state dictionary as `.pt` file.
     """
-    model.save_encoder(model_path)
+    if hasattr(model.encoder,'state_dict'):
+        torch.save(model.encoder.state_dict(),model_path)
+    else: model.save_encoder(model_path)
 
 
 def decoder_saver(model, model_path: str) -> None:
@@ -70,7 +72,9 @@ def decoder_saver(model, model_path: str) -> None:
     Returns:
         None: Saved decoder state dictionary as `.pt` file.
     """
-    model.save_decoder(model_path)
+    if hasattr(model.decoder,'state_dict'):
+        torch.save(model.decoder.state_dict(),model_path)
+    else: model.save_decoder(model_path)
 
 
 def initialise_model(model_name: str):
