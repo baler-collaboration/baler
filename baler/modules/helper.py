52f0fa965a5a7863401160a0bf4fa2ce8b93b328
Pratik Jawahar <prjawaha@lxplus984.cern.ch>
2024-06-06 12:43:55 +0200

    fixing missing config flags

--- baler/modules/helper.py
+++ baler/modules/helper.py
@@ -179,6 +179,8 @@ class Config:
     emd: bool
     l1: bool
     deterministic_algorithm: bool
+    separate_model_saving: bool
+    save_error_bounded_deltas: bool
 
 
 def create_default_config(workspace_name: str, project_name: str) -> str:
@@ -230,6 +232,7 @@ def set_config(c):
     c.activation_extraction        = False
     c.deterministic_algorithm      = True
     c.separate_model_saving        = False
+    c.save_error_bounded_deltas   = False
 
 """
 
@@ -1078,52 +1081,4 @@ class KANLinear(torch.nn.Module):
         return (
             regularize_activation * regularization_loss_activation
             + regularize_entropy * regularization_loss_entropy
-        )
-
-
-class KAN(torch.nn.Module):
-    def __init__(
-        self,
-        layers_hidden,
-        grid_size=5,
-        spline_order=3,
-        scale_noise=0.1,
-        scale_base=1.0,
-        scale_spline=1.0,
-        base_activation=torch.nn.SiLU,
-        grid_eps=0.02,
-        grid_range=[-1, 1],
-    ):
-        super(KAN, self).__init__()
-        self.grid_size = grid_size
-        self.spline_order = spline_order
-
-        self.layers = torch.nn.ModuleList()
-        for in_features, out_features in zip(layers_hidden, layers_hidden[1:]):
-            self.layers.append(
-                KANLinear(
-                    in_features,
-                    out_features,
-                    grid_size=grid_size,
-                    spline_order=spline_order,
-                    scale_noise=scale_noise,
-                    scale_base=scale_base,
-                    scale_spline=scale_spline,
-                    base_activation=base_activation,
-                    grid_eps=grid_eps,
-                    grid_range=grid_range,
-                )
-            )
-
-    def forward(self, x: torch.Tensor, update_grid=False):
-        for layer in self.layers:
-            if update_grid:
-                layer.update_grid(x)
-            x = layer(x)
-        return x
-
-    def regularization_loss(self, regularize_activation=1.0, regularize_entropy=1.0):
-        return sum(
-            layer.regularization_loss(regularize_activation, regularize_entropy)
-            for layer in self.layers
         )
